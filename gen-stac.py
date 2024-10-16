7b40e6feb2a38992e169664c44cad49da1e91e24
Benjamin Clark <clarkben@meta.com>
2024-10-16 11:33:45 -0400

    Get the stac assembled, but writing it out still needs to be massaged.

--- gen-stac.py
+++ gen-stac.py
@@ -6,9 +6,26 @@ import os
 import json
 import yaml
 import requests
+import pystac
+from datetime import datetime
 
 #release_version = "2024-06-13-beta.1"
 release_version = "2024-09-18.0"
+
+license_dict = {
+    "base": 'ODbL',
+    "buildings": 'ODbL',
+    "divisions": 'ODbL',
+    "transportation":'ODbL',
+    "places": 'CDLA Permissive 2.0',
+    "addresses": 'Multiple permissive open licenses'
+}
+
+
+def get_release_date_time():
+    release_date_str = release_version.split('.')[0]
+    fmt_str = '%Y-%m-%d'
+    return datetime.strptime(release_date_str, fmt_str)
 
 
 # Parse the schema-to-release mapping yaml available on the Overture Maps github org, and cross-reference it with the 
@@ -71,8 +88,9 @@ def get_type_parquet_bbox(s3fs, filepath):
 def parse_name(s3_file_path): 
     return os.path.split(s3_file_path)[1].split('=')[1]
 
+
 # Generate the type-specific blocks that go in the theme-level of the manifest
-def process_type(s3fs, type_info, type_name, theme_relative_path):
+def process_type(theme_catalog, s3fs, type_info, type_name, theme_relative_path):
     type_dict = {}
     type_dict['name'] = type_name;
     print ("Processing " + type_name + " type")
@@ -80,6 +98,14 @@ def process_type(s3fs, type_info, type_name, theme_relative_path):
     rel_path = '/' + os.path.split(type_info.path)[1]
     type_dict['relative_path'] = rel_path
     type_info = s3fs.get_file_info(theme_path_selector)
+    ## To do: do we need to be more precise with our extent here? 
+    extent = pystac.SpatialExtent(bboxes=[[[-180.0, -90.0, 180.0, 90.0]]])
+    type_collection = pystac.Collection(
+        id=type_name, 
+        description='Type information', 
+        extent = extent,
+        license = 'ODbL'
+    )
 
     files = []
     for type in type_info: 
@@ -93,16 +119,29 @@ def process_type(s3fs, type_info, type_name, theme_relative_path):
             type_info_obj['name'] = type_filename
 
             # extract the bbox that covers this particular file's worth of data
-            type_info_obj['bbox'] = get_type_parquet_bbox(s3fs, release_path + theme_relative_path + rel_path + "/" + type_filename)
+            file_path = release_path + theme_relative_path + rel_path + "/" + type_filename
+            type_info_obj['bbox'] = get_type_parquet_bbox(s3fs, file_path)
 
             files.append(type_info_obj)
-
+            stac_item = pystac.Item(id=type_filename,geometry=None, bbox=type_info_obj['bbox'], properties={}, datetime=get_release_date_time())
+            stac_item.add_asset(
+                key='parquet-'+type_filename,
+                asset=pystac.Asset(href=file_path,
+                media_type = 'application/vnd.apache.parquet')
+            )
+            type_collection.add_item(stac_item)
         get_type_schema_info(s3fs, release_path + theme_relative_path + rel_path)
     type_dict['files'] = files
+    theme_catalog.add_child(type_collection)
+    print ('Type Collection description: ')
+    type_collection.describe()
+    #print("Type Collection: " + json.dumps(type_collection.to_dict(), indent=4))
+
+
     return type_dict
 
 # Generate the theme-specific blocks that go in the top-line manifest
-def process_theme(s3fs, theme_info, theme_name):
+def process_theme(release_catalog, s3fs, theme_info, theme_name):
     theme_dict = {}
     theme_dict['name'] = theme_name;
     print ("\tProcessing theme " + theme_name)
@@ -114,13 +153,17 @@ def process_theme(s3fs, theme_info, theme_name):
     theme_dict['status'] = '{alpha/beta/release}'
     theme_info = s3fs.get_file_info(theme_path_selector)
     type_info = []
+    theme_catalog = pystac.Catalog(id=theme_name, description='Theme information');
 
     for type in theme_info:
         if (not type.is_file):
             type_name = parse_name(type.path)
-            type_info.append(process_type(filesystem, type, type_name, rel_path))
-    
+            type_info.append(process_type(theme_catalog, filesystem, type, type_name, rel_path))
     theme_dict['types'] = type_info
+    release_catalog.add_child(theme_catalog)
+
+#    print("Theme Catalog: " + json.dumps(theme_catalog.to_dict(), indent=4))
+
     return theme_dict
 
 print ('Generating release manifest for release ' + release_version)
@@ -136,19 +179,20 @@ themes_info = filesystem.get_file_info(release_path_selector);
 
 theme_info = []
 
+release_catalog = pystac.Catalog(id='release', description='This catalog is for the geoparquet data released as version ' + release_version);
+
 for theme in themes_info:
     theme_name = parse_name(theme.path) 
-    theme_info.append(process_theme(filesystem, theme, theme_name))
+    #for now just short-circuit the process to work on addresses
+    if theme_name == 'addresses':
+        theme_info.append(process_theme(release_catalog, filesystem, theme, theme_name))
 
-json_dict['themes'] = theme_info
+#print("Type Collection: " + json.dumps(release_catalog.to_dict(), indent=4))
 
-json_object = json.dumps(json_dict, indent=4)
+print("Release Catalog description:")
+release_catalog.describe()
 
-with open("sample.json", "w") as outfile:
-    outfile.write(json_object)
-
-# dataset = ds.dataset(
-#     path, filesystem=fs.S3FileSystem(anonymous=True, region="us-west-2")
-# )
-
-
+release_catalog.normalize_and_save(
+    root_href='./sample-stac.json', 
+    catalog_type=pystac.CatalogType.SELF_CONTAINED
+)
